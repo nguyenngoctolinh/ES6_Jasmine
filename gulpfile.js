b2bbe831230b68b42ebfe2004e3f4af8fb37648b
nguyenngoctolinh <linhnguyen@qasymphony.com>
2016-08-25 06:57:28 +0700

    framework with nodemailer, zip, report

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@ var gulp = require('gulp');
 // import gulp from 'gulp'
 var webdriver = require('gulp-webdriver');
 // import webdriver from 'gulp-webdriver'
-
-gulp.task('default', () => {
+var shell = require('gulp-shell');
+gulp.task('test', () => {
   return gulp.src('wdio.conf.js').pipe(webdriver());
-})
+});
+
+gulp.task('report', () => {
+  return gulp.src('wdio.conf.js')
+    .pipe(shell([
+      'allure generate reports',
+      'allure report open'
+    ]));
+});
 
 
-
+gulp.task('default', ['test', 'report']);
--- src/utils/zip.js
+++ src/utils/zip.js
@@ -1,21 +1,3 @@
-// import jszip from 'jszip'
-// var JSZip = require('jszip');
-// var fileSaver = require('file-saver');
-
-// var zip = new JSZip();
-
-// zip.folder("logs");
-// zip.generateAsync({ type: "uint8array" })
-//   .then((uint8array) => {
-//     saveAs(uint8array, "./logs.zip");
-//   })
-//   .then(() => {
-//     console.log('zip successfully');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 var file_system = require('fs');
 var archiver = require('archiver');
 
@@ -34,7 +16,7 @@ archive.on('error', function(err){
 archive.pipe(output);
 archive.bulk([
     { expand: true, 
-      cwd: 'logs', //folder which you want to zip
+      cwd: 'reports', //folder which you want to zip
       src: ["**/*"], 
       dest: ''
     }
