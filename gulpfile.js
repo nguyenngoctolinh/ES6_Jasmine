9568b6fe78c795913febe3dc4083403077609335
nguyenngoctolinh <linhnguyen@qasymphony.com>
2016-08-30 20:21:47 +0700

    report and sendmail with ip computer URL

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,15 @@ gulp.task('default', () => {
   return gulp.src('wdio.conf.js').pipe(webdriver());
 });
 
-gulp.task('report', () => {
+gulp.task('reportSM', () => {
   return gulp.src('wdio.conf.js')
     .pipe(shell([
       'allure generate reports',
-      'allure report open -p 8080'
-    ]));
-});
-
-gulp.task('sendmail', () => {
-  return gulp.src('wdio.conf.js')
-    .pipe(shell([
       'node ./src/utils/zip.js',
-      'node ./src/utils/send_mail.js'
-    ]));
+      'node ./src/utils/send_mail.js',
+      'allure report open -p 8080'
 
+    ]));
 });
 
 
--- src/utils/send_mail.js
+++ src/utils/send_mail.js
@@ -1,28 +1,46 @@
 var nodemailer = require("nodemailer");
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var addresses = [];
+const port = 8080;
+var hostName = '';
 
-let transporter = nodemailer.createTransport('smtps://es6jasmine%40gmail.com:es6_jasmine@smtp.gmail.com');
+var transporter = nodemailer.createTransport('smtps://es6jasmine%40gmail.com:es6_jasmine@smtp.gmail.com');
+
+showIP = () => {
+  for (let k in interfaces) {
+    for (let k2 in interfaces[k]) {
+      let address = interfaces[k][k2];
+      if (address.family === 'IPv4' && !address.internal) {
+        addresses.push(address.address);
+      }
+    }
+  }
+  hostName = addresses[0] + ":" + port;
+}
+showIP();
+
 let mailOptions = {
   from: '"ES6 Jasmine 👥" <es6jasmine@gmail.com>',
   to: '"Techcon 2016 👥" <techconsept2016@gmail.com>',
   subject: 'Techcon 10th Sept, 2016 Report',
-  html: `<p>Dear Techcon 2016, <br><br>
+  html: `<p>Dear Techcon 2016, <br/><br/>
    We have run completely the test suite of Login cases. <br> 
-   Please take a look at the attached file for detail. <br><br>
-   Thanks and Regards, <br><br>
+   Please take a look at the attached file and the report link: ${hostName}   for detail. <br/><br/>
+   Thanks and Regards, <br/><br/>
    ES6 & Jasmine</p>`,
 
-    attachments: [
-      {
-        filename: 'reports.zip',
-        path: './reports.zip'
-      }
-    ]
-};
+  attachments: [
+    {
+      filename: 'reports.zip',
+      path: './reports.zip'
+    }
+  ]
+}; 
+
 transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
     return console.log(error);
   }
   console.log('Message sent: ' + info.response);
 });
-
-
